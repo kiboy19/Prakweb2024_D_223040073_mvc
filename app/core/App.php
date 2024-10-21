3b426f0a86f8e0234e418745b7251b1154c914e1
kiboy19 <arielseptiadi3@gmail.com>
2024-10-21 09:10:49 +0700

    Update Belajar MVC - Video ke 4 (Belum selesai menit ke 7:26)

--- app/core/App.php
+++ app/core/App.php
@@ -1,10 +1,21 @@
 <?php
 
 class App {
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
     public function __construct()
     {
         $url = $this->parseURL();
-        var_dump($url);
+        
+        if( file_exists('../app/controllers/' . $url[0] . '.php')) {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+        
+        require_once 'file../app/controllers/' . $this->controller .'.php';
+        $this->controller = new $this->controller;
     }
 
     public function parseURL()
