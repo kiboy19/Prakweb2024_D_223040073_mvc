5fc9e451d6b094aa41acf1e9b1710bbd0a7ea15f
Muhamad Ariel Septiadi <arielseptiadi3@gmail.com>
2024-10-27 03:21:38 +0700

    Update Belajar MVC - Video ke 6 Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,7 @@
-<h1> About Me </h1>
-<p> Halo, nama saya <?= $data['nama']; ?>, Umur saya <?= $data['umur']; ?> tahun,
-    saya adalah seorang <?= $data['pekerjaan'] ?>.</p>
+<div class="container">
+
+    <h1 class="mt-4">About Me </h1>
+    <img src="<?= BASEURL; ?>/img/kiboy.jpeg" alt="Ibnu" width="200" class="rounded-circle shadow">
+    <p> Halo, nama saya <?= $data['nama']; ?>, Umur saya <?= $data['umur']; ?> tahun,
+        saya adalah seorang <?= $data['pekerjaan'] ?>.</p>
+</div>
