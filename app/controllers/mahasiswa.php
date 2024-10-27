612343fe078ddfc9a4df08fc9c4b271c53c7cb95
Muhamad Ariel Septiadi <arielseptiadi3@gmail.com>
2024-10-27 15:30:10 +0700

    Update Belajar MVC - Video ke 8 Insert Data

--- app/controllers/mahasiswa.php
+++ app/controllers/mahasiswa.php
@@ -9,4 +9,11 @@ class mahasiswa extends Controller
         $this->view('mahasiswa/index', $data);
         $this->view('tempelates/footer');
     }
+    public function tambah()
+    {
+        if ($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0) {
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        }
+    }
 }
